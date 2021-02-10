6ba056ef385ba7f4f246e2c5af3bf282d1038889
andyheko <haoen.ko@gmail.com>
2021-02-10 14:38:22 -0800

    add QueryFiniteStateTests.java file

---
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/query/QueryFiniteStateTests.java
@@ -0,0 +1,22 @@
+package org.springframework.data.mongodb.core.query;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.data.mongodb.core.query.Criteria.*;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+
+import org.bson.Document;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+
+public class QueryFiniteStateTests {
+    @Test
+    void testQueryAddCriteria() {
+        Query q = new Query(where("name").is("Thomas").and("age").lt(80));
+        q.addCriteria(where("value").is("1"));
+        assertThat(q.getQueryObject()).isEqualTo(Document.parse("{\"name\" : \"Thomas\", \"age\" : { \"$lt\" : 80}, \"value\" : \"1\"}"));
+    }
+}
