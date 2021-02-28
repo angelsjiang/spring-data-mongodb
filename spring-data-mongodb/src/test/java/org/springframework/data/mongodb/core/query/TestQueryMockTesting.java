549d9dea2405d6edeb0ae78d6b322c3301f815f9
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2021-02-28 11:56:21 -0800

    edited mock testing

--- spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/query/TestQueryMockTesting.java
+++ spring-data-mongodb/src/test/java/org/springframework/data/mongodb/core/query/TestQueryMockTesting.java
@@ -2,18 +2,25 @@ package org.springframework.data.mongodb.core.query;
 
 import org.bson.Document;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.springframework.data.mongodb.test.util.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class TestQueryMockTesting {
 
     @Mock
     TextCriteria textCriteriaMock;
+
+    @BeforeEach
+    void setUp() {
+        textCriteriaMock = TextCriteria.forLanguage("Spanish");
+    }
 
     @Test
     public void testTextQuery() {
@@ -31,14 +38,19 @@ public class TestQueryMockTesting {
         TextQuery tq = TextQuery.queryText(textCriteriaMock);
         tq.includeScore();
 
-
         assertThat(tq.getQueryObject()).containsEntry("$text.$language", LANGUAGE_SPANISH);
         assertThat(tq.getQueryObject()).containsEntry("$text.$search", QUERY);
         assertThat(tq.getQueryObject()).containsEntry("$text.$caseSensitive", true);
         assertThat(tq.getQueryObject()).containsEntry("$text.$diacriticSensitive", false);
         assertTrue(tq.getFieldsObject().containsKey("score"));
 
+        tq.setScoreFieldName("foo");
+
+        assertEquals(tq.getScoreFieldName(), "foo");
+        assertFalse(tq.getFieldsObject().containsKey("score"));
     }
+
+
 
     private static final String QUERY = "cake";
     private static final String LANGUAGE_SPANISH = "Spanish";
